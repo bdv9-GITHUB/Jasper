f4530b0b654622313256bf2005f5f57a48efafaf
bdv9-GITHUB <birdv76@gmail.com>
2020-10-16 00:01:49 +0300

    configering dependencies + export to excel

--- src/main/java/jasper/JasperToXls.java
+++ src/main/java/jasper/JasperToXls.java
@@ -2,6 +2,8 @@ package jasper;
 
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.export.*;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,31 +16,39 @@ public class JasperToXls {
     // в файл Excel (xlsFile)
     // с заполнением данными из переданного источника (dataSource)
     @SuppressWarnings("unchecked")
-    public static void exportToXls(String jrxmlFile, String xlsFile, JRDataSource dataSource) throws JRException {
+    public static void exportToXls(String jrxmlFile,
+                                   String xlsFile,
+                                   JRDataSource dataSource) throws JRException {
         // Готовим параметры для отчета (заголовок и т.д.)
         Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("ReportTitle", "Коммерческое предложение");
 
         // Компилируем jrxml в бинарный формат jasper
-        //String jrxmlFile = "c:\\temp\\TextReport.jrxml";
         JasperReport jasperReport = JasperCompileManager.compileReport(jrxmlFile);
 
         // Получаем отчет на основе шаблона (jrxml) и коллекции с данными
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        JasperPrint jasperPrint = JasperFillManager
+                .fillReport(jasperReport, parameters, dataSource);
 
-        /*// Экспорт в pdf
-        String pdfFile = "c:\\temp\\TextReport.pdf";
-        JasperExportManager.exportReportToPdfFile(jasperPrint, pdfFile);*/
+        // Экспорт в pdf
+//        String pdfFile = "c:\\temp\\TextReport.pdf";
+//        JasperExportManager.exportReportToPdfFile(jasperPrint, pdfFile);
 
         // Отображение в стандартном просмотрщике
-        /*JasperViewer jasperViewer = new JasperViewer(jasperPrint);
-        jasperViewer.setVisible(true);*/
+//        JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+//        jasperViewer.setVisible(true);
 
         // Готовим экспорт в Excel
-        //String xlsFile = "c:\\temp\\TextReport.xls";
         JRXlsExporter exporter = new JRXlsExporter();
-        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
-        exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, xlsFile);
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(xlsFile));
+        SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
+        configuration.setOnePagePerSheet(true);
+        configuration.setDetectCellType(true);
+        configuration.setCollapseRowSpan(false);
+        exporter.setConfiguration(configuration);
+
+        // Экспорт в Excel
         exporter.exportReport();
     }
 }
